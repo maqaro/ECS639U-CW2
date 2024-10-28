1c9087bcc7d28d0b3a3fa96cbbbb08292799c2e3
maqaro <basilnasr@outlook.com>
2024-10-28 21:49:44 +0000

    added team model

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    captain = models.CharField(max_length=100)
+    coach = models.CharField(max_length=100)
+    year_founded = models.IntegerField(default=1900)
+
+    def __str__(self):
+        return f"{self.name} - ({self.city})"
+    
+    # class League(models.Model):
+    #     name = models.CharField(max_length=100)
+    #     country = models.CharField(max_length=100)
+    #     teams = models.ManyToManyField(Team, related_name='teams')
+
+    #     def __str__(self):
+    #         return f"{self.name} - ({self.country})"
