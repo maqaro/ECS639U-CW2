ba9e91bc47c048056fecfc610bc442c724b8e0af
maqaro <basilnasr@outlook.com>
2024-10-28 23:25:35 +0000

    finished lecture 1

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index_view
+from .views import index_view, team_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('team/<int:team_id>', team_view, name='team'),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Team
 
 def index_view(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return render(request, 'main/index.html', {
+        'teams': Team.objects.all()
+    }) 
+
+def team_view(request, team_id):
+    return render(request, 'main/team.html', {
+        'team': Team.objects.get(id=team_id) 
+    })
