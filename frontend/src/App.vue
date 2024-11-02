4ebe15b87405faa9e466b7dfe08887a2da984982
maqaro <basilnasr@outlook.com>
2024-11-02 01:44:02 +0000

    got vue working (for now)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,50 +1,148 @@
 <template>
     <div class="container pt-3">
         <div class="h1 text-center border rounded bg-light p-2 mb-3">
-            API Client
+            {{ title }}
         </div>
-        <div class="mb-3">
-            <u>Response data</u>:             
-        </div>
-        <pre>{{ response_data }}</pre>
 
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
+        <button class="btn btn-sm btn-success mb-3" data-bs-toggle="modal" data-bs-target="#addTeamModal">
+            <i class="bi bi-plus"></i> Add Team 
         </button>
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal fade" id="addTeamModal" tabindex="-1" aria-labelledby="addTeamModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="addTeamModalLabel">{{ isEditing ? 'Edit Team' : 'Add Team' }}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input v-model="newTeam.name" type="text" class="form-control" id="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="city" class="form-label">City</label>
+                            <input v-model="newTeam.city" type="text" class="form-control" id="city">
+                        </div>
+                        <div class="mb-3">
+                            <label for="captain" class="form-label">Captain</label>
+                            <input v-model="newTeam.captain" type="text" class="form-control" id="captain">
+                        </div>
+                        <div class="mb-3">
+                            <label for="coach" class="form-label">Coach</label>
+                            <input v-model="newTeam.coach" type="text" class="form-control" id="coach">
+                        </div>
+                        <div class="mb-3">
+                            <label for="year_founded" class="form-label">Year Founded</label>
+                            <input v-model="newTeam.year_founded" type="number" class="form-control" id="year_founded">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            Close
+                        </button>
+                        <button type="button" class="btn btn-primary" @click="isEditing ? updateTeam() : createTeam()" data-bs-dismiss="modal">
+                            Save
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-            </div>
-        </div>
         </div>
 
+        <TeamTable 
+            :teams="teams"
+            @delete-team="deleteTeam"
+            @edit-team="editTeam"
+        />
     </div>
-  </template>
+</template>
   
 <script>
+import TeamTable from './components/TeamTable.vue'
+
+const baseUrl = 'http://localhost:8000'
+
 export default {
+    components: {
+        TeamTable
+    },
     data() {
         return {
-            response_data: '',
+            title: 'Team Information',
+            teams: [],
+            newTeam: {
+                name: '',
+                city: '',
+                captain: '',
+                coach: '',
+                year_founded: 1900
+            },
+            isEditing: false,
+            editingTeamId: null
         }
     },
     async mounted() {
-        const response = await fetch('http://localhost:8000/api/test.json')
-        this.response_data = await response.json()
+        const response = await fetch(`${baseUrl}/api/teams/`)
+        const data = await response.json()
+        this.teams = data.teams;
+    },
+    methods: {
+        async deleteTeam(team) {
+            if (confirm(`Are you sure you want to delete team '${team.name}'?`)) {
+                const response = await fetch(`${baseUrl}/api/teams/${team.id}/`, {
+                    method: 'DELETE',
+                })
+                if (response.ok)
+                    this.teams = this.teams.filter(t => t.id !== team.id)
+            }
+        },
+        async createTeam() {
+            const response = await fetch(`${baseUrl}/api/teams/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.newTeam)
+            })
+            const newTeam = await response.json()
+            this.teams.push(newTeam)
+            this.resetForm()
+        },
+        async updateTeam() {
+            const response = await fetch(`${baseUrl}/api/teams/${this.editingTeamId}/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.newTeam)
+            })
+            const updatedTeam = await response.json()
+            const index = this.teams.findIndex(t => t.id === this.editingTeamId)
+            this.teams.splice(index, 1, updatedTeam)
+            this.resetForm()
+        },
+        editTeam(team) {
+            this.isEditing = true
+            this.editingTeamId = team.id
+            this.newTeam = { ...team }
+            const modal = new bootstrap.Modal(document.getElementById('addTeamModal'))
+            modal.show()
+        },
+        resetForm() {
+            this.newTeam = {
+                name: '',
+                city: '',
+                captain: '',
+                coach: '',
+                year_founded: 1900
+            }
+            this.isEditing = false
+            this.editingTeamId = null
+        }
     }
 }
 </script>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/TeamTable.vue
@@ -0,0 +1,58 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Team</th>
+                <th scope="col">City</th>
+                <th scope="col">Captain</th>
+                <th scope="col">Coach</th>
+                <th scope="col">Year Founded</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(team, index) in teams" :key="team.id">
+                <th scope="row">{{ index + 1 }}</th>
+                <td>{{ team.name }}</td>
+                <td>{{ team.city }}</td>
+                <td>{{ team.captain }}</td>
+                <td>{{ team.coach }}</td>
+                <td>{{ team.year_founded }}</td>
+                <td>
+                    <button 
+                        class="btn btn-sm btn-primary me-2"
+                        @click="editTeam(team)"
+                    >
+                        <i class="bi bi-pencil-square"></i>
+                    </button>
+                    <button 
+                        class="btn btn-sm btn-danger"
+                        @click="$emit('delete-team', team)"
+                    >
+                        <i class="bi bi-trash"></i>
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: {
+        teams: Array
+    },
+    methods: {
+        editTeam(team) {
+            this.$emit('edit-team', team);
+        }
+    }
+}
+</script>
+
+<style scoped>
+td, th {
+    vertical-align: middle;
+}
+</style>
